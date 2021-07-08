d83eec9432d6ccd63550d3ef36bd68e4d57fab43
en-i <templebond@icloud.com>
2021-07-08 23:57:29 +0900

    Initial Commit

---
+++ My Janken/My_JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_JankenApp.swift
+//  My Janken
+//
+//  Created by terada enishi on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct My_JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
